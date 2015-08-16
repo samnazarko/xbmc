e39ecae13b4acb5781db4f6a4f2af0e13d10a66a
Sam Nazarko <email@samnazarko.co.uk>
2015-08-16 07:43:03 +0100

    Do not return SCM revision in version
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/utils/SystemInfo.cpp
+++ xbmc/utils/SystemInfo.cpp
@@ -1350,8 +1350,7 @@ std::string CSysInfo::GetVersionShort()
 
 std::string CSysInfo::GetVersion()
 {
-  return GetVersionShort() + " (" + CCompileInfo::GetVersionCode() + ")" +
-         " Git:" + CCompileInfo::GetSCMID();
+  return GetVersionShort();
 }
 
 std::string CSysInfo::GetVersionCode()
