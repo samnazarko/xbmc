1b2275c4d8e9c395a2f4e17c1823380fc45a0621
Sam Nazarko <email@samnazarko.co.uk>
2018-02-03 18:42:49 +0000

    allowsharing needs to be set for mounted drives to be streamed properly. See https://discourse.osmc.tv/t/yatse-and-osmc-2017-12-1-local-streaming/70370/24. Pre-define a source and make sure they are always set if there is no sources.xml (first-run).
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -361,6 +361,7 @@ bool CApplication::Create()
   CUtil::CopyUserDataIfNeeded("special://masterprofile/", "RssFeeds.xml");
   CUtil::CopyUserDataIfNeeded("special://masterprofile/", "favourites.xml");
   CUtil::CopyUserDataIfNeeded("special://masterprofile/", "Lircmap.xml");
+  CUtil::CopyUserDataIfNeeded("special://masterprofile/", "sources.xml");
 
   CServiceBroker::GetLogging().Initialize(CSpecialProtocol::TranslatePath("special://logpath"));
 
