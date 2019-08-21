3b0a5e91cb4d8a1c81649ad8b1409745c3ea7ee1
tanio99 <tanio99@wolke7.net>
2019-08-21 10:25:22 +0200

    amlcodec fix: set correct video format for MVC content

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.h
@@ -73,6 +73,7 @@ public:
   virtual void SetSpeed(int iSpeed) override;
   virtual void SetCodecControl(int flags) override;
   virtual const char* GetName(void) override { return (const char*)m_pFormatName; }
+  virtual bool SupportsExtention() { return true; }
 
 protected:
   void            Dispose(void);
