6efee21cad4af8a9da0999e7af17f560ddb840d0
tanio99 <tanio99@wolke7.net>
2022-12-27 09:45:25 +0100

    AMLInsecureVideoCodec: fixed typo

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/amlogic/AMLInsecureVideoCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/amlogic/AMLInsecureVideoCodec.cpp
@@ -1253,7 +1253,7 @@ void AMLInsecureVideoCodec::setVideoRect(const CRect &SrcRect, const CRect &Dest
 
 	if (m_guiStereoMode == RENDER_STEREO_MODE_MONO) {
 		std::string videoStereoMode = m_processInfo.GetVideoStereoMode();
-		if (videoStereoMode == "left_right" || videoStereoMode == "righ_left")
+		if (videoStereoMode == "left_right" || videoStereoMode == "right_left")
 			dst_rect.x2 *= 2.0;
 		else if (videoStereoMode == "top_bottom" || videoStereoMode == "bottom_top")
 			dst_rect.y2 *= 2.0;
