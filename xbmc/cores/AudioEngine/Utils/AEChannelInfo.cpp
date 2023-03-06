38408ec8a50ef3ca8645772facdd02f52ca36543
Sam Nazarko <email@samnazarko.co.uk>
2023-03-06 16:59:35 +0000

    Add support for outputting formats in their proper container format. When ALSA sink can't be opened in this mode, we mask channels via HDMI TX instead. Updated to work around USB audio quirks
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/cores/AudioEngine/Utils/AEChannelInfo.cpp
+++ xbmc/cores/AudioEngine/Utils/AEChannelInfo.cpp
@@ -313,7 +313,8 @@ bool CAEChannelInfo::ContainsChannels(const CAEChannelInfo& rhs) const
 {
   for (unsigned int i = 0; i < rhs.m_channelCount; ++i)
   {
-    if (!HasChannel(rhs.m_channels[i]))
+   if (rhs.m_channels[i] > AE_CH_NULL && rhs.m_channels[i] < AE_CH_UNKNOWN1 &&
+        !HasChannel(rhs.m_channels[i]))
       return false;
   }
   return true;
