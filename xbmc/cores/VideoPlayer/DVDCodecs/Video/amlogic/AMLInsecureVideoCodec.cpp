25879eacb366cef0494a69a7a60fc1b16a6eee5c
tanio99 <tanio99@wolke7.net>
2024-03-30 11:34:26 +0100

    AMLInsecureVideoCodec: fix video rate correction for 25 fps playback

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/amlogic/AMLInsecureVideoCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/amlogic/AMLInsecureVideoCodec.cpp
@@ -224,7 +224,7 @@ unsigned int AMLInsecureVideoCodec::getVideoRate(const CDVDStreamInfo &hints) co
 	// incorrectly reported as 50 fps (yes, video_rate == 1920)
 	if (hints.width == 1920 && video_rate == 1920) {
 		CLog::Log(LOGDEBUG, "AMLInsecureVideoCodec::getVideoRate: video_rate exception");
-		video_rate = 0.5 + (float) UNIT_FREQ * 1001 / 25000;
+		video_rate = 0.5 + (float) UNIT_FREQ * 1000 / 25000;
 	}
 
 	return video_rate;
